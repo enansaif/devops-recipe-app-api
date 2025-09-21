0d2f0ec6a3fbc53b1da478146564cb9487925f8e
enansaif <enansaifme33@gmail.com>
2025-09-21 11:12:02 +0600

    added output.tf and validated the iam terraform

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -1,6 +1,6 @@
-##########################################
-# Create IAM user and policies for ci/cd #
-##########################################
+#######################################################################
+# Create IAM user and policies for Continuous Deployment (CD) account #
+#######################################################################
 
 resource "aws_iam_user" "cd" {
   name = "recipe-app-api-cd"
@@ -10,43 +10,44 @@ resource "aws_iam_access_key" "cd" {
   user = aws_iam_user.cd.name
 }
 
-###########################################################
-# Policy for terraform backend to s3 and dynamo db access #
-###########################################################
+#########################################################
+# Policy for Teraform backend to S3 and DynamoDB access #
+#########################################################
 
 data "aws_iam_policy_document" "tf_backend" {
   statement {
-    effect = "Allow"
-    actions = ["s3:ListBucket"]
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
     resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
   }
+
   statement {
-    effect = "Allow"
+    effect  = "Allow"
     actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
     resources = [
-        "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
-        "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
     ]
   }
   statement {
     effect = "Allow"
     actions = [
-        "dynamodb:DescribeTable",
-        "dynamodb:GetItem",
-        "dynamodb:PutItem",
-        "dynamodb:DeleteItem"
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
     ]
-    resources = "arn:aws:dynamodb:*:*:table/${var.tf_state_lock_table}"
+    resources = ["arn:aws:dynamodb:*:*:table/${var.tf_state_lock_table}"]
   }
 }
 
 resource "aws_iam_policy" "tf_backend" {
-  policy = data.aws_iam_policy_document.tf_backend.json
-  name = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
-  description = "Allow user to use s3 and DynamoDB for TF backend resources"
+  name        = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
+  description = "Allow user to use S3 and DynamoDB for TF backend resources"
+  policy      = data.aws_iam_policy_document.tf_backend.json
 }
 
 resource "aws_iam_user_policy_attachment" "tf_backend" {
-  policy_arn = aws_iam_policy.tf_backend
-  user = aws_iam_user.cd.name
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.tf_backend.arn
 }
